442ebc6a7332b915b70c867421862e5a904c91b5
mong9ssi <mong9ssi@naver.com>
2025-04-17 10:52:49 +0900

    Lv 1 변수선언을 while문 밖으로 수정 , 사칙연산 구현

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -38,10 +38,11 @@ public class Main {
 
         // boolean 정의
         boolean bool1 = true;
+        int int1 = 0;
         // 반복문 while 사용해서 bool1 = true 일떄 if 값까지 진행 / false 일데 if else 값 진행
         while (bool1) {
             System.out.println("원하는 첫번째 숫자를 입력하세요 : ");
-            int int1 = scanner.nextInt();
+            int1 = scanner.nextInt();
             if (int1 < 0) {
                 System.out.println("음수를 입력했습니다. 양수(0포함)를 입력해주세요."); // 음수일 때 실행
             } else {
@@ -51,10 +52,11 @@ public class Main {
         }
 
         boolean bool2 = true;
+        int int2 = 0;
 
         while (bool2) {
             System.out.println("원하는 두번째 숫자를 입력하세요 : ");
-            int int2 = scanner.nextInt();
+            int2 = scanner.nextInt();
             if (int2 <0 ) {
                 System.out.println("음수를 입력했습니다. 양수(0포함)를 입력해주세요."); // 음수일 때 실행
             } else {
@@ -63,25 +65,49 @@ public class Main {
             }
         }
 
+
         // ㅁ 사칙연산 기호(+,-,*,/) 를 입력받기
         //    ㅁScanner를 사용하여 사칙연산 기호를 전달 받을 수 있습니다.
         //    ㅁ사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다. ( charAt(0) )
 
-
         boolean bool3 = true;
+        char char1 = '+';
 
         while (bool3) {
             System.out.println("원하는 사칙연산을 입력하세요 ( + , - , * , / ) : ");
             String string1 = scanner.next();
             if(string1.equals("+")|| string1.equals("-")|| string1.equals("*")|| string1.equals("/")) {
-                char char1 = string1.charAt(0); // 사칙연산 기호로 저장
+                char1 = string1.charAt(0); // 사칙연산 기호로 저장
                 System.out.println("입력한 사칙연산 : " + char1);
                 break;
             } else {
-                System.out.println("잘못 입력했습니다. 다시한번 입력해주세요.");
+                System.out.println("잘못 입력했습니다. 사칙연산 기호에 맞게 다시 한번 입력해주세요.");
             }
         }
 
-        
+
+        // ㅁ 위에서 입력받은 양의 정수 2개와 사칙연산 기호를 사용하여 연산을 진행한 후 결과값을 출력하기
+        //     ㅁ 키워드 : if switch
+        //     ㅁ 사칙연산 기호에 맞는 연산자를 사용하여 연산을 진행합니다.
+        //     ㅁ 입력받은 연산 기호를 구분하기 위해 제어문을 사용합니다. (예를 들면 if, switch)
+        //     ㅁ 연산 오류가 발생할 경우 해당 오류에 대한 내용을 정제하여 출력합니다.
+        //       ex) “나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.“
+
+        //   int1 = 첫번째 입력받은 수 // int2 = 두번째 입력받은 수 // char1 = 입력받은 사칙연산
+
+        if(char1 == '+') {
+            System.out.println(int1 + int2);
+        } else if (char1 == '-') {
+            System.out.println(int1 - int2);
+        } else if (char1 == '*') {
+            System.out.println(int1 * int2);
+        } else if (char1 == '/') {
+            if(int2 == 0) {
+                System.out.println("나눗셈 연산에서 분보(두번째 정수)에 0이 입력될 수 없습니다.");
+            }
+            System.out.println(int1 / int2);
+        }
+
+
     }
 }
