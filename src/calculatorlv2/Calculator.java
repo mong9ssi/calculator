9eea16236ca2982483c0c98ad5dc46352b7adf9a
mong9ssi <mong9ssi@naver.com>
2025-04-22 21:11:20 +0900

    Lv 2 Calculator 클래스에서 사칙연산 기능 활용해 메인 클래스에 적용

--- src/calculatorlv2/Calculator.java
+++ src/calculatorlv2/Calculator.java
@@ -9,66 +9,29 @@ public class Calculator {
     private double double1;
     private double double2;
     private char char1;
-    private List<Double> resultList = new ArrayList<>();
 
     // 2. 생성자
-    Calculator() {}
-
-    Calculator(double double1 , double double2){
+    // 기본 사칙연산 생성자.
+    public Calculator(double double1 , double double2){
         this.double1 = double1;
         this.double2 = double2;
     }
 
-
     // 3. 기능
-
-//    // 덧셈
-//    double sum (double double1 , double double2) {
-//        return double1 + double2;
-//    }
-//    //뺼셈
-//    double sub (double double1 , double double2) {
-//        return double1 - double2;
-//    }
-//    //곱셈
-//    double mul (double double1 , double double2) {
-//        return double1 * double2;
-//    }
-//    //나눗셈
-//    double div (double double1 , double double2) {
-//        return double1 / double2;
-//    }
-
     // 사칙연산
     double arithmetic (double double1 , double double2 , char char1) {
         switch (char1) {
-            case '+' :
+            case '+':
                 return double1 + double2;
-            case '-' :
+            case '-':
                 return double1 - double2;
-            case '*' :
+            case '*':
                 return double1 * double2;
-            case '/' :
-                if (double2 == 0) {
-                    System.out.println("0으로 나눌 수 없습니다");
-                } else {
-                    return double1 / double2;
-                }
-            default:
-                System.out.println("사칙연산(+,-,*,/) 을 입력해주세요");
-        }
-        return 0;
+            case '/':
+                return double1 / double2;
+        } return 0;
     }
 
-
-    //리스트 추가 기능.
-    void addresult(double result) {
-        resultList.add(result);
-    }
-
-    List<Double> getResultList() {
-        return resultList;
-    }
 
 
 }
--- src/calculatorlv2/Main.java
+++ src/calculatorlv2/Main.java
@@ -6,15 +6,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Calculator!");
 
-        //스캐너 생성
-        Scanner scanner = new Scanner(System.in);
-        //List 저장
-        Calculator calculatorResult = new Calculator();
-
-        // int , char 정의
+        // int , char 사전 정의
         double double1 = 0;
         double double2 = 0;
         char char1 = '+';
+
+        //스캐너 생성
+        Scanner scanner = new Scanner(System.in);
 
         // 사칙연산 생성자 생성
         Calculator calculator = new Calculator(double1 , double2);
@@ -37,6 +35,7 @@ public class Main {
             //    ㅁScanner 를 사용하여 사칙연산 기호를 전달 받을 수 있습니다.
             //    ㅁ사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다. ( charAt(0) )
 
+            // 사칙연산 잘못 입력시 처음으로 반복함 , 제대로 입력시 char 로 치환 후 반복문 종료
             while (true) {
                 System.out.println("원하는 사칙연산을 입력하세요 ( + , - , * , / ) : ");
                 String string1 = scanner.next();
@@ -54,11 +53,11 @@ public class Main {
                 double2 = scanner.nextInt();
                 if (double2 < 0) {
                     System.out.println("음수를 입력했습니다. 양수(0포함)를 입력해주세요."); // 음수일 때 실행
-                } else if (double2 == 0 && char1 == '/') {
-                    System.out.println("0으로 나눌 수 없습니다 다시 입력해주세요.");
+                } else if (char1 == '/' && double2 == 0) {
+                    System.out.println("0으로 나눌 수 없습니다 다시 입력해주세요."); // 0으로 나누는 경우 팝업 발생 후 처음부터 재실행
                 } else {
                     System.out.println("두번째 숫자 : " + double2);
-                    break; // 양수일 때 끝냄
+                    break;
                 }
             }
 
@@ -70,29 +69,31 @@ public class Main {
             //     ㅁ 연산 오류가 발생할 경우 해당 오류에 대한 내용을 정제하여 출력합니다.
             //       ex) “나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.“
 
-            if (char1 == '+'){
-                double result = calculator.arithmetic(double1 , double2 , char1);
-                System.out.println("결과 : " + double1 + char1 + double2 + " = " + result);
-                calculatorResult.addresult(result);
-            } else if (char1 == '-') {
-                double result = calculator.arithmetic(double1 , double2 , char1);
-                System.out.println("결과 : " + double1 + " - " + double2 + " = " + result);
-                calculatorResult.addresult(result);
-            } else if (char1 == '*') {
-                double result = calculator.arithmetic(double1 , double2 , char1);
-                System.out.println("결과 : " + double1 + " * " + double2 + " = " + result);
-                calculatorResult.addresult(result);
-            } else if (char1 == '/') {
-                if (double2 != 0) {
-                    double result = calculator.arithmetic(double1 , double2 , char1);
-                    System.out.println("결과 : " + double1 + " / " + double2 + " = " + result);
-                    calculatorResult.addresult(result);
-                } else {
-                    System.out.println("나눗셈 연산에서 분보(두번째 정수)에 0이 입력될 수 없습니다.");
-                }
-            }
+//            if (char1 == '+'){
+//                double result = calculator.arithmetic(double1 , double2 , char1);
+//                System.out.println("결과 : " + double1 + char1 + double2 + " = " + result);
+//                calculatorResult.addresult(result);
+//            } else if (char1 == '-') {
+//                double result = calculator.arithmetic(double1 , double2 , char1);
+//                System.out.println("결과 : " + double1 + " - " + double2 + " = " + result);
+//                calculatorResult.addresult(result);
+//            } else if (char1 == '*') {
+//                double result = calculator.arithmetic(double1 , double2 , char1);
+//                System.out.println("결과 : " + double1 + " * " + double2 + " = " + result);
+//                calculatorResult.addresult(result);
+//            } else if (char1 == '/') {
+//                if (double2 != 0) {
+//                    double result = calculator.arithmetic(double1 , double2 , char1);
+//                    System.out.println("결과 : " + double1 + " / " + double2 + " = " + result);
+//                    calculatorResult.addresult(result);
+//                } else {
+//                    System.out.println("나눗셈 연산에서 분보(두번째 정수)에 0이 입력될 수 없습니다.");
+//                }
+//            }
+            double result = calculator.arithmetic(double1 , double2 , char1); // Scanner 통해 입력받은 값으로 생성자 생성
+            System.out.println("결과 : " + double1 + char1 + double2 + " = " + result);
 
-
+            // 연산 진행 후 계속 진행 여부 확인
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료 / 아무키나 입력시 처음으로)");
             String string1 = scanner.next();
             if (string1.equals("exit")) {
